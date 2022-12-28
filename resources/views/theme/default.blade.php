f358bb3abef5be1b8e65503068a9890990bebc91
falah <masterwarefun@gmail.com>
2022-12-28 12:13:05 +0700

    first commit

---
+++ resources/views/theme/default.blade.php
@@ -0,0 +1,337 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <title>GAS - Welcome to Gas.id</title>
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+    <meta content="" name="keywords">
+    <meta content="" name="description">
+
+    <!-- Favicon -->
+    <link href="img/favicon.ico" rel="icon">
+
+    <!-- Google Web Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700&family=Open+Sans:wght@400;500;600;700&display=swap" rel="stylesheet">
+
+    <!-- Icon Font Stylesheet -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.10.0/css/all.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.1/font/bootstrap-icons.css" rel="stylesheet">
+
+    <!-- Libraries Stylesheet -->
+    <link href="lib/animate/animate.min.css" rel="stylesheet">
+    <link href="lib/owlcarousel/assets/owl.carousel.min.css" rel="stylesheet">
+
+    <!-- Customized Bootstrap Stylesheet -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Template Stylesheet -->
+    <link href="css/style.css" rel="stylesheet">
+</head>
+
+<body style="background-color: #FEEA00;">
+    <div class="container-xxl bg-white p-0">
+        <!-- Spinner Start -->
+        <div id="spinner" class="show bg-white position-fixed translate-middle w-100 vh-100 top-50 start-50 d-flex align-items-center justify-content-center">
+            <div class="spinner-border text-primary" style="width: 3rem; height: 3rem;" role="status">
+                <span class="sr-only">Loading...</span>
+            </div>
+        </div>
+        <!-- Spinner End -->
+
+
+        <!-- Navbar & Hero Start -->
+        <div class="container-xxl position-relative p-0">
+            <nav style="background-color: #FEEA00;" class="navbar navbar-expand-lg navbar-light px-4 px-lg-5 py-3 py-lg-0">
+                <a href="" class="navbar-brand p-0">
+                    <img style="margin-left: 190px;" src="https://gas.id/files/images/srvphp_1662943062.png" alt="Logo">
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse">
+                    <span class="fa fa-bars"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarCollapse">
+                    <div class="navbar-nav ms-auto py-0">
+                        <a href="index.html" class="nav-item nav-link active">Beranda</a>
+                        <a href="about.html" class="nav-item nav-link active">Tentang GAS</a>
+                        <a href="domain.html" class="nav-item nav-link active">Fitur</a>
+                        <a href="hosting.html" class="nav-item nav-link active">Daftar</a>
+                        <a href="hosting.html" class="nav-item nav-link active">Karir</a>
+                    </div>
+                    <a href="" class="btn btn-secondary py-2 px-4 ms-3">Masuk Mitra</a>
+                </div>
+            </nav>
+
+            <div class="container-xxl py-5 bg-primary">
+                <div class="container my-5 py-5 px-lg-5">
+                    <div class="row g-5">
+                        <div class="col-lg-6 pt-5 text-center text-lg-start">
+                            <h1 style="color: #104D7F !important;" class="display-4 text-white mb-4 animated slideInLeft">Bergabung dengan GAS</h1>
+                            <p style="color: #104D7F !important;" class="text-white animated slideInLeft">Aplikasi untuk memudahkan pelaku usaha menjangkau calon pembeli lebih luas. Daftarkan tokomu dan nikmati keuntungan bersama GAS!</p>
+                            <a href="" class="btn btn-secondary py-sm-3 px-sm-5 me-3 animated slideInLeft">Unduh Sekarang</a>
+                        </div>
+                        <div class="col-lg-6 text-center text-lg-start">
+                            <img class="img-fluid animated zoomIn" src="https://gas.id/files/images/ilustration_top_1616732601.png" alt="">
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- Navbar & Hero End -->
+
+
+
+
+
+
+
+        <!-- About Start -->
+        <div style="background-color:#144D95" class="container-xxl py-5">
+            <div class="container px-lg-5">
+                <div class="row g-5 align-items-center">
+                    <div class="col-lg-7 wow fadeInUp" data-wow-delay="0.1s">
+                        <h4 style="color: white;" class="mb-4">GAS adalah aplikasi untuk memudahkan pelaku usaha menjangkau calon pembeli lebih luas. Melalui gerakan #AyoBelanjaTokoTetangga GAS mengajak untuk menjelajah dan mendukung usaha-usaha di sekitarmu.</h4>
+                        <div class="row g-3">
+                            <div class="col-sm-4 wow fadeIn" data-wow-delay="0.1s">
+                                <div>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-5">
+                        <img class="img-fluid wow zoomIn" data-wow-delay="0.5s" style="width: 260px;" src=" img/1.png">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- About End -->
+
+        <!-- About Start -->
+        <div style="background-color:#FDFDFD" class="container-xxl py-5">
+            <div class="container px-lg-5">
+                <div class="row g-5 align-items-center">
+                    <div class="col-lg-7 wow fadeInUp" data-wow-delay="0.1s">
+                        <h4 style="color: #104D7F;" class="mb-4">Ayo Nikmati Beragam Fitur GAS dan Kembangkan Usahamu Jadi Toko Online</h4>
+                        <div class="row g-3">
+                            <div class="col-sm-4 wow fadeIn" data-wow-delay="0.1s">
+                                <div>
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-lg-5">
+                        <img class="img-fluid wow zoomIn" data-wow-delay="0.5s" style="width: 260px;" src="img/2.png">
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- About End -->
+
+
+        <!-- Pricing Start -->
+        <div class="container-xxl py-5">
+            <div class="container px-lg-5">
+                <div style="/* max-width: 600px; */visibility: visible;animation-delay: 0.1s;animation-name: fadeInUp;" class="section-title position-relative text-center mx-auto mb-5 pb-4 wow fadeInUp" data-wow-delay="0.1s" style="max-width: 600px;">
+                    <h4 class="mb-3">Apa pun jenis tokomu bisa bergabung bersama GAS</h4>
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/VxVWACx9NIY" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+                </div>
+            </div>
+        </div>
+        <style>
+            .box_jobs {
+                margin: 20px 15px;
+                display: inline-flex;
+            }
+
+            .button_job {
+                text-align: right;
+            }
+
+            .button_job p {
+                background-color: #d4a90a;
+                display: inline-block;
+                padding: 5px;
+                border-radius: 10px;
+                cursor: pointer;
+                color: #232222;
+            }
+
+            .button_job p:hover {
+                background-color: #efc014;
+            }
+
+            /*display: inline-flex;display: flex;float: left;*/
+        </style>
+        <!-- Pricing End -->
+        <!-- Pricing Start -->
+        <div style="background-color:#144D95" class="container-xxl py-5">
+            <div class="container px-lg-5">
+                <div style="/* max-width: 600px; */visibility: visible;animation-delay: 0.1s;animation-name: fadeInUp;" class="section-title position-relative text-center mx-auto mb-5 pb-4 wow fadeInUp" data-wow-delay="0.1s" style="max-width: 600px;">
+                    <h4 style="color: white;" class="mb-3">Temukan Karier Impianmu
+                    </h4>
+                    <p style="color: white;margin-bottom: 100px !important;" class="mb-4">Mari bergabung bersama kami. Jadilah bagian dari kami dan kembangkan bakat, kreatifmu di GAS</p>
+                    <div style="color: white;" class="row g-5 comparison position-relative">
+                        <div class="col-lg-6 pe-lg-5">
+                            <div class="row gy-3 gx-5">
+                                <div class="box_jobs">
+                                    <div style="padding-right: 10px;" class="image_jobs">
+                                        <img src="https://gas.id/files/images/digitalmarketing_1616663371.png">
+                                    </div>
+                                    <div class="box_desc_job">
+                                        <div class="title_job">
+                                            <h4 style="text-align: left; color:#FEEA00" class="font-m">Marketing Communication</h4>
+                                        </div>
+                                        <div style="text-align: left;" class="desc_job">
+                                            <p>Merancang strategi komunikasi pemasaran mulai dari branding, iklan dan pemasaran langsung.</p>
+                                        </div>
+                                        <div class="button_job font-lite" onclick="open_descjob('Marketing_Communication')">
+                                            <p>Baca Selengkapnya</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="box_jobs">
+                                    <div style="padding-right: 10px;" class="image_jobs">
+                                        <img src="https://gas.id/files/images/customerservice_1616663487.png">
+                                    </div>
+                                    <div class="box_desc_job">
+                                        <div class="title_job">
+                                            <h4 style="text-align: left; color:#FEEA00" class="font-m">Customer Service</h4>
+                                        </div>
+                                        <div style="text-align: left;" class="desc_job">
+                                            <p>Menjawab telepon, menanggapi pelanggan melalui email, dan memberikan informasi terkait layanan.</p>
+                                        </div>
+                                        <div class="button_job font-lite" onclick="open_descjob('Marketing_Communication')">
+                                            <p>Baca Selengkapnya</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="box_jobs">
+                                    <div style="padding-right: 10px;" class="image_jobs">
+                                        <img src="https://gas.id/files/images/administration_1616663583.png">
+                                    </div>
+                                    <div class="box_desc_job">
+                                        <div class="title_job">
+                                            <h4 style="text-align: left; color:#FEEA00" class="font-m">Administration</h4>
+                                        </div>
+                                        <div style="text-align: left;" class="desc_job">
+                                            <p>Membuat arsip data perusahaan, mengatur agenda kantor, dan menyusun laporan keuangan perusahaan..</p>
+                                        </div>
+                                        <div class="button_job font-lite" onclick="open_descjob('Marketing_Communication')">
+                                            <p>Baca Selengkapnya</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-lg-6 ps-lg-5">
+                            <div class="row gy-3 gx-5">
+                                <div class="box_jobs">
+                                    <div style="padding-right: 10px;" class="image_jobs">
+                                        <img src="https://gas.id/files/images/contentwriter_1616663403.png">
+                                    </div>
+                                    <div class="box_desc_job">
+                                        <div class="title_job">
+                                            <h4 style="text-align: left; color:#FEEA00" class="font-m">Content Writer</h4>
+                                        </div>
+                                        <div style="text-align: left;" class="desc_job">
+                                            <p>Membuat dan mengelola konten web atau artikel yang dapat membangun branding perusahaan..</p>
+                                        </div>
+                                        <div class="button_job font-lite" onclick="open_descjob('Marketing_Communication')">
+                                            <p>Baca Selengkapnya</p>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="box_jobs">
+                                    <div style="padding-right: 10px;" class="image_jobs">
+                                        <img src="https://gas.id/files/images/iosdeveloper_1616663544.png">
+                                    </div>
+                                    <div class="box_desc_job">
+                                        <div class="title_job">
+                                            <h4 style="text-align: left; color:#FEEA00" class="font-m">IOS Developer</h4>
+                                        </div>
+                                        <div style="text-align: left;" class="desc_job">
+                                            <p>Pengalaman mengembangkan aplikasi IOS (minimal 4 aplikasi dipublikasikan). Akrab dengan platform Swift atau Hybrid.</p>
+                                        </div>
+                                        <div class="button_job font-lite" onclick="open_descjob('Marketing_Communication')">
+                                            <p>Baca Selengkapnya</p>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+        <!-- Pricing End -->
+
+        <!-- Footer Start -->
+        <div class="container-fluid bg-primary text-white wow fadeIn" data-wow-delay="0.1s">
+            <div class="container py-5 px-lg-5">
+                <div class="row gy-5 gx-4 pt-5">
+
+                    <div class="col-lg-5 col-md-12">
+                        <div class="row gy-5 g-4">
+                            <div style="color: black !important;" class="col-md-6">
+                                <h5 style="color: black !important;" class="fw-bold text-white mb-4">Tentang GAS</h5>
+                                <a style="color: black !important;" class="" href="">Terms and Conditions</a>
+                                <a style="color: black !important;" class="" href="">Privacy</a>
+                            </div>
+                            <div style="color: black !important;" class="col-md-6">
+                                <h5 style="color: black !important;" class="fw-bold text-white mb-4">Hubungi Kami</h5>
+                                <a style="color: black !important;" class="" href="">Kritik Saran</a>
+                            </div>
+
+                        </div>
+                    </div>
+                    <div class="col-md-6 col-lg-3">
+                        <h5 style="color: black !important;" class="fw-bold text-white mb-4">Ikuti Kami</h5>
+                        <p class="mb-2"><img style="
+    width: 30px;
+    padding-right: 5px;
+" src="https://gas.id/assets/icon_fcb.png"><img style="
+    width: 30px;
+    padding-right: 5px;
+" src="https://gas.id/assets/icon_ing.png"><img style="
+    width: 30px;
+    padding-right: 5px;
+" src="https://gas.id/assets/youtube-symbol.png"></p>
+                    </div>
+                    <div class="col-md-6 col-lg-3">
+                        <h5 style="color: black !important;" class="fw-bold text-white mb-4">Unduh</h5>
+                        <div class="bodycontent appsimg_footer">
+                            <img style="width: 105px;" src="https://gas.id/assets/play_store.png">
+                            <img style="width: 105px;" src="https://gas.id/assets/app_store.png">
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+        <!-- Footer End -->
+
+
+        <!-- Back to Top -->
+        <a href="#" class="btn btn-lg btn-secondary btn-lg-square back-to-top"><i class="bi bi-arrow-up"></i></a>
+    </div>
+
+    <!-- JavaScript Libraries -->
+    <script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="lib/wow/wow.min.js"></script>
+    <script src="lib/easing/easing.min.js"></script>
+    <script src="lib/waypoints/waypoints.min.js"></script>
+    <script src="lib/counterup/counterup.min.js"></script>
+    <script src="lib/owlcarousel/owl.carousel.min.js"></script>
+
+    <!-- Template Javascript -->
+    <script src="js/main.js"></script>
+</body>
+
+</html>
